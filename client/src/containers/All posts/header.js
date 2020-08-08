626d01ca183f570558bf85e1eb7e4b168d5ac642
Niraj shrestha <CrestNiraj12>
2020-08-08 15:05:22 +0545

    starting with frontend

---
+++ client/src/containers/All posts/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header header__home">
+      <div className="header__content-wrapper">
+        <h1>Hello world!</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- client/src/containers/Home/index.js
+++ client/src/containers/Home/index.js
@@ -1,35 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { setPage } from "../../actions";
-import Header from "./header";
+import React, { useEffect } from "react";
 import Content from "./content";
-import Footer from "../../components/Footer";
+import store from "../../store";
+import { setPage } from "../../actions";
 import { HOME } from "../../constants";
-import { connect } from "react-redux";
 
-const mapStateToProps = (state) => ({
-  navActive: state.navActive,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setPage: (page) => dispatch(setPage(page)),
-});
-
-const Home = ({ navActive, setPage }) => {
-  const [activeNav, setActiveNav] = useState(false);
-
+const Portfolio = () => {
   useEffect(() => {
-    setPage(HOME);
-    setActiveNav(navActive);
-  }, [navActive, setPage]);
-
-  return (
-    <>
-      {activeNav && <div className="body__overlay"></div>}
-      <Header />
-      <Content />
-      <Footer />
-    </>
-  );
+    store.dispatch(setPage(HOME));
+  }, []);
+  return <Content />;
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Portfolio;
