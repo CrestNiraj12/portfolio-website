a154317cadddfb6e81a0574411643ddae527e7f3
Niraj shrestha <CrestNiraj12>
2020-08-13 21:40:22 +0545

    Pushing

--- server.js
+++ server.js
@@ -68,12 +68,11 @@ connection
   })
   .catch((err) => console.log(err));
 
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build", index.html));
-  });
-}
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client/build", index.html));
+});
 
 app.listen(port, () => console.log("Server running on port: " + port));
