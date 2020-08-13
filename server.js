cfdc34dffb947c6530609b2f330fc772c65fc62e
Niraj shrestha <CrestNiraj12>
2020-08-13 21:00:46 +0545

    bug fix

--- server.js
+++ server.js
@@ -70,9 +70,9 @@ connection
 
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", index.html));
+    res.sendFile(path.resolve(__dirname, "client/build", index.html));
   });
 }
 
