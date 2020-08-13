d91bbf6c66542b523fa77fd7a3da683bde8be4c7
Niraj shrestha <CrestNiraj12>
2020-08-14 02:16:27 +0545

    changes

---
+++ client/build/precache-manifest.d4cd9921a522b44508d31fa746c724d7.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70be345ba9dd3880f2d35cd945d641e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6c0fb089a316e2aa353",
+    "url": "/static/css/main.fb505bf2.chunk.css"
+  },
+  {
+    "revision": "7036d7b9a45916295ef6",
+    "url": "/static/js/2.657d2ac8.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/static/js/2.657d2ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c0fb089a316e2aa353",
+    "url": "/static/js/main.26768133.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4425d5fe53212256a1a30ce6d2333cfe",
+    "url": "/static/media/add.4425d5fe.svg"
+  },
+  {
+    "revision": "d81ff9922238ae01ac10c8167dfc7a05",
+    "url": "/static/media/arrow-link.d81ff992.svg"
+  },
+  {
+    "revision": "e30801a0183fe1c2ffb3ad081f014b7c",
+    "url": "/static/media/arrow.e30801a0.svg"
+  },
+  {
+    "revision": "b05186c2f5ff4a424abcba13428bcf50",
+    "url": "/static/media/at.b05186c2.gif"
+  },
+  {
+    "revision": "240938714b0916cff5422f910507dcdd",
+    "url": "/static/media/avatar.24093871.svg"
+  },
+  {
+    "revision": "67d5fa8cd7d9f670afe06471d7aba20c",
+    "url": "/static/media/candidate.67d5fa8c.svg"
+  },
+  {
+    "revision": "67f88ac6a8e855c2883a7b6601925616",
+    "url": "/static/media/close-eye.67f88ac6.svg"
+  },
+  {
+    "revision": "26eed9ec385dafc3dea0e990bd9deed6",
+    "url": "/static/media/close-icon.26eed9ec.svg"
+  },
+  {
+    "revision": "c6fe4b41e03cdbfc4e61227e688604ac",
+    "url": "/static/media/control.c6fe4b41.svg"
+  },
+  {
+    "revision": "09efbae4bac8a78f6f00fff0ab1e704a",
+    "url": "/static/media/download.09efbae4.svg"
+  },
+  {
+    "revision": "bf8bf80bf6a36bf2d7cf81f4b02d6e13",
+    "url": "/static/media/eye.bf8bf80b.svg"
+  },
+  {
+    "revision": "0801d8b200a1b69abd156babfcb3eac9",
+    "url": "/static/media/fb.0801d8b2.svg"
+  },
+  {
+    "revision": "5ae8d5b195efa19927142e4eaa77021b",
+    "url": "/static/media/github.5ae8d5b1.svg"
+  },
+  {
+    "revision": "99992053676ab6b5ff28ffec24d500dd",
+    "url": "/static/media/home.99992053.svg"
+  },
+  {
+    "revision": "171af6475aa9d3f6dd780fdbb8c81030",
+    "url": "/static/media/html.171af647.svg"
+  },
+  {
+    "revision": "4ab9d652506085cc626bcc5244d6241a",
+    "url": "/static/media/info.4ab9d652.svg"
+  },
+  {
+    "revision": "7089cd85e8f19cce92707e7105620f7a",
+    "url": "/static/media/insta.7089cd85.svg"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/static/media/js.0cded3a3.png"
+  },
+  {
+    "revision": "11df45ab599f3a6cbfac21198a838ea7",
+    "url": "/static/media/link.11df45ab.svg"
+  },
+  {
+    "revision": "249fec1a55a773d074ec46b31127b795",
+    "url": "/static/media/logo.249fec1a.png"
+  },
+  {
+    "revision": "a7df3742f413da4146b2582259a08475",
+    "url": "/static/media/more.a7df3742.svg"
+  },
+  {
+    "revision": "76cdd0780251160569de126c66e1a21b",
+    "url": "/static/media/nodejs.76cdd078.svg"
+  },
+  {
+    "revision": "a28e551ee9c45f2ef693308081f5adf7",
+    "url": "/static/media/paper.a28e551e.svg"
+  },
+  {
+    "revision": "c5b14b1692e6f3963fbeba9be583517d",
+    "url": "/static/media/pencil.c5b14b16.svg"
+  },
+  {
+    "revision": "c998e0093fa89602b3f796b5d81c820b",
+    "url": "/static/media/project.c998e009.jpg"
+  },
+  {
+    "revision": "e2f8c511a19e1ab8e878bd0bddd75f11",
+    "url": "/static/media/python.e2f8c511.svg"
+  },
+  {
+    "revision": "be5818ca111391d6c8bdcc44b168aea3",
+    "url": "/static/media/react.be5818ca.svg"
+  },
+  {
+    "revision": "5e748447495821eb636e266e53d034d7",
+    "url": "/static/media/redux.5e748447.svg"
+  },
+  {
+    "revision": "ea6862b96545861f67c5accd46e8ee3e",
+    "url": "/static/media/trash.ea6862b9.svg"
+  }
+]);
