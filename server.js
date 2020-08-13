18555fb46ea71e4e5eea82f75dfe03c7d9bbdcc0
Niraj shrestha <CrestNiraj12>
2020-08-14 02:12:54 +0545

    small changes

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ if (process.env.NODE_ENV === "production") {
   const path = require("path");
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve("client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
