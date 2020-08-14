40449a1622cd77557265bde253b992407a788196
CrestNiraj12 <crestniraj@gmail.com>
2020-08-15 04:57:52 +0545

    test link gen

--- backend/config/utils/linkGenerator.js
+++ backend/config/utils/linkGenerator.js
@@ -1,13 +1,13 @@
 module.exports = (req, path, token) => {
   const port = process.env.PORT || 3000;
-  console.log(port);
+  console.log(typeof port);
   console.log(port === 80);
   console.log(path);
   return (
     req.protocol +
     "://" +
     (process.env.NODE_ENV === "production" ? req.get("host") : "localhost") +
-    (port === 80 || port === 443 ? "" : `:${port}`) +
+    (port === "80" || port === "443" ? "" : `:${port}`) +
     path +
     token
   );
