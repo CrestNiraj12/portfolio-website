fce27f2f81c25eff1337ae8c22acd3f20c8a3844
CrestNiraj12 <crestniraj@gmail.com>
2020-08-14 15:51:22 +0545

    test deloy #1

--- backend/config/utils/linkGenerator.js
+++ backend/config/utils/linkGenerator.js
@@ -1,11 +1,13 @@
 module.exports = (req, path, token) => {
   const port = process.env.PORT || 3000;
-
+  console.log(req.get("host"));
   return (
     req.protocol +
     "://" +
-    (process.env.NODE_ENV === "production" ? req.get("host") : "localhost") +
-    (port === 80 ? "" : `:${port}`) +
+    (process.env.NODE_ENV === "production"
+      ? req.get("host").split(":")[0]
+      : "localhost") +
+    (port === 80 || port === 443 ? "" : `:${port}`) +
     path +
     token
   );
