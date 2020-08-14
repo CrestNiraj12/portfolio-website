1062bfc3193013024e43c4088f4de0b001db4680
CrestNiraj12 <crestniraj@gmail.com>
2020-08-15 03:11:38 +0545

    bug fix try #1

--- server.js
+++ server.js
@@ -22,6 +22,12 @@ app.use(cors());
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production")
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+
 app.use(express.static(__dirname + "/client/public"));
 
 const url = process.env.CONNECTION_URL;
