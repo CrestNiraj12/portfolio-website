9c4405c7a1c674c1ec8b9deb8a57149adbaa8b5c
CrestNiraj12 <crestniraj@gmail.com>
2020-08-14 13:19:49 +0545

    stable

--- server.js
+++ server.js
@@ -80,8 +80,7 @@ connection
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    console.log("here");
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
