511223ebecdf877206b2266d3b6952b1b11d240c
CrestNiraj12 <crestniraj@gmail.com>
2020-08-20 16:08:38 +0545

    crop button customized

--- client/src/components/CustomButton/index.js
+++ client/src/components/CustomButton/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { ReactComponent as ButtonLoading } from "./svg/buttonLoading.svg";
 
-const CustomButton = ({ loading, text, clsName, handleClick }) => {
+const CustomButton = ({ loading, text, clsName, handleClick, svgStyle }) => {
   return (
     <button
       onClick={handleClick}
       disabled={loading}
       className={clsName ? clsName : ""}
     >
-      {loading && <ButtonLoading className="button-loading__svg" />}
+      {loading && (
+        <ButtonLoading className="button-loading__svg" style={svgStyle} />
+      )}
       {!loading && <span>{text}</span>}
     </button>
   );
